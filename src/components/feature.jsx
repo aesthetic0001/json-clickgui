bb102b6face9f3bf201e6f68f503934d03692543
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-06-13 08:27:28 -0400

    removed hover scale

--- src/components/feature.jsx
+++ src/components/feature.jsx
@@ -1,7 +1,7 @@
 export default function Feature({name, description, section, children}) {
     return (
         <div
-            className="bg-sidebar-bg p-3 mx-3 h-fit rounded-2xl hover:scale-[102%] group transition-all ease-in-out select-none"
+            className="bg-sidebar-bg p-3 mx-3 h-fit rounded-2xl group transition-all ease-in-out select-none"
             tabIndex={0}>
             <h1 className="text-2xl group-hover:text-text-active text-text-primary transition-all ease-in-out">{name} <span className="text-xl text-gray-600">({section})</span></h1>
             <p className="text-lg text-gray-500">{description}</p>
