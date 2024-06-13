a29498541d2f2de0e12bbbbd2f61d9ee0461adf8
aesthetic0001 <15858616+aesthetic0001@users.noreply.github.com>
2024-06-12 20:24:20 -0400

    on hover instead focus for feature

--- src/components/feature.jsx
+++ src/components/feature.jsx
@@ -1,20 +1,8 @@
-import {useState} from "react";
-import clsx from "clsx";
-
 export default function Feature({ name, description, section }) {
-    const [active, setActive] = useState(false);
-
     return (
-        <div className="flex-grow bg-sidebar-bg p-3 mx-3 rounded-2xl hover:scale-[102%] transition-all ease-in-out shadow-2xl select-none"
-                onFocus={() => setActive(true)}
-                onBlur={() => setActive(false)}
+        <div className="flex-grow bg-sidebar-bg p-3 mx-3 rounded-2xl hover:scale-[102%] hover:text-text-active text-text-primary transition-all ease-in-out select-none"
                 tabIndex={0}>
-            <h1 className={
-                clsx(
-                    "text-2xl transition-all ease-in-out",
-                    active ? "text-text-active" : "text-text-primary"
-                )
-            }>{name} <span className="text-xl text-gray-600">({section})</span></h1>
+            <h1 className="text-2xl">{name} <span className="text-xl text-gray-600">({section})</span></h1>
             <p className="text-lg text-gray-500">{description}</p>
         </div>
     );
